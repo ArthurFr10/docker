cf49ac6b47cadee9c0f47c5e1f37fee758623f53
ArthurFr10 <arthurfragabbmp@gmail.com>
2024-11-13 19:43:24 +0000

    salvar

---
+++ app/services/usuario_service.py
@@ -0,0 +1,27 @@
+from models.usuario_model import Usuario
+from repositories.usuario_repository import UsuarioRepository
+
+class UsuarioService:
+    def __init__(self, repository: UsuarioRepository):
+        self.repository = repository
+
+    def criar_usuario(self, nome:str, email:str, senha:str):
+        try:
+            usuario = Usuario(nome=nome, email=email, senha=senha)
+            
+            usuario_cadastrado = self.repository.pesquisar_usuario_por_email(usuario.email)
+
+            if usuario_cadastrado:
+                print("Usuário já cadastrado!")
+                return
+
+            self.repository.salvar_usuario(usuario)
+            print("Usuário já cadastrado!")
+        except TypeError as erro:
+            print(f"Erro ao salvar usuário: {erro}")
+        except Exception as erro:
+            print(f"Ocorreu um erro inesperado: {erro}")
+
+    def listar_todos_usuarios(self):
+        return self.repository.listar_usuarios()
+
